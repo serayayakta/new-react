cf27c6eaf88ba97b61b1df647d0399e1ebe8b42d
Seray Ayakta <seray.ayakta@teamdefinex.com>
2022-08-16 19:31:59 +0300

    Simple dropdown select component is created and used in page

--- my-app/src/components/SimpleDropdownSelect.js
+++ my-app/src/components/SimpleDropdownSelect.js
@@ -1,1 +1,29 @@
-import Select from "react-dropdown-select";
+import React, { Component } from 'react';
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const techCompanies = [
+    { label: "Apple", value: 1 },
+    { label: "Facebook", value: 2 },
+    { label: "Netflix", value: 3 },
+    { label: "Tesla", value: 4 },
+    { label: "Amazon", value: 5 },
+    { label: "Alphabet", value: 6 },
+];
+
+export default class SimpleDropdownSelect extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                        <Select options={techCompanies} />
+                    </div>
+                    <div className="col-md-4"></div>
+                </div>
+            </div>
+        )
+    }
+}
--- my-app/src/pages/DropdownPage.js
+++ my-app/src/pages/DropdownPage.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import Select from 'react-select';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const techCompanies = [
-  { label: "Apple", value: 1 },
-  { label: "Facebook", value: 2 },
-  { label: "Netflix", value: 3 },
-  { label: "Tesla", value: 4 },
-  { label: "Amazon", value: 5 },
-  { label: "Alphabet", value: 6 },
-];
+import SimpleDropdownSelect from '../components/SimpleDropdownSelect';
 
 const DropdownPage = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-md-4"></div>
-      <div className="col-md-4">
-        <Select options={ techCompanies } />
-      </div>
-      <div className="col-md-4"></div>
+    <div className="container">
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Dropdown Options</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Check each one</h6>
+                <div className="row">
+                    <div className="col-md-4">
+                        <SimpleDropdownSelect />
+                    </div>
+                    <div className="col-md-4">
+                        <SimpleDropdownSelect />
+                    </div>
+                    <div className="col-md-4">
+                        <SimpleDropdownSelect />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 );
 
 export default DropdownPage
